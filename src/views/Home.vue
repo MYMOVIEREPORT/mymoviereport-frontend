28f032383785cc0802921681d8df2789a64f63a8
MANDARIN <compurain@naver.com>
2019-11-23 19:59:50 +0900

    홈 포스트리스트파트 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
-  <div>메인 홈페이지</div>
+  <div>
+    <h3>(공개된)포스트리스트</h3>
+    <hr />
+    <PostList :posts="posts" />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import PostList from "../components/Post/PostList";
+import axios from "axios";
 export default {
   name: "home",
-  components: {}
+  components: {
+    PostList
+  },
+  data() {
+    return {
+      posts: null
+    };
+  },
+  methods: {
+    getPublishedPosts() {
+      const reqUrl = "http://localhost:8000";
+      axios
+        .get(`${reqUrl}/api/v1/posts/`)
+        .then(res => {
+          const { data } = res;
+          this.posts = data;
+        })
+        .catch(err => console.log(err));
+    }
+  },
+  mounted() {
+    this.getPublishedPosts();
+  }
 };
 </script>
