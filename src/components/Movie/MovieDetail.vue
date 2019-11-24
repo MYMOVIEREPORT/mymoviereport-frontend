4cbd056b8ff994f74bcc747d26f552359122c8f2
MANDARIN <compurain@naver.com>
2019-11-24 15:14:18 +0900

    영화 관련 포스트 생성 + 이미 평가 했는지 여부 확인 + 반영

--- src/components/Movie/MovieDetail.vue
+++ src/components/Movie/MovieDetail.vue
@@ -1,59 +1,56 @@
 <template>
   <div>
-    <div v-if="loading">loading</div>
-    <div v-else>
-      <!-- 영화 디테일 -->
-      <h1 class="d-inline">{{movie.title_ko}}</h1>
-      <h3 class="text-muted font-italic d-inline">{{movie.title ? movie.title : '-'}}</h3>
-      <hr />
+    <!-- 영화 디테일 -->
+    <h1 class="d-inline">{{movie.title_ko}}</h1>
+    <h3 class="text-muted font-italic d-inline">{{movie.title ? movie.title : '-'}}</h3>
+    <hr />
 
-      <div class="row mb-3">
-        <!-- 포스터 -->
-        <div class="col">
-          <img :src="movie.poster_url ? movie.poster_url : blankImageUrl" style="width:100%" />
-        </div>
-        <!-- 영화 세부 내용 -->
-        <div class="col">
-          <!-- 장르(일단 대표장르만 하나) -->
-          <h3 class="text-center">⭐️ {{movie.score}}</h3>
-          <h5 class="text-center">{{movie.genre.name}}</h5>
-          <!-- 감독'들' -->
-          <div class="text-center mb-2">
-            <h6 class="m-0">감독</h6>
-            <span v-if="!movie.directors">-</span>
-            <span
-              v-else
-              class="mr-1"
-              v-for="director in movie.directors"
-              :key="'director-' + director.id"
-            >{{director.name}}</span>
-          </div>
-          <!-- 배우'들' -->
-          <div class="text-center">
-            <h6 class="m-0">배우</h6>
-            <span v-if="!movie.actors">-</span>
-            <span
-              v-else
-              class="mr-1"
-              v-for="actor in movie.actors"
-              :key="'actor-'+ actor.id"
-            >{{actor.name}}</span>
-          </div>
-          <hr />
-          <p>{{movie.description ? movie.description :'-'}}</p>
-        </div>
+    <div class="row mb-3">
+      <!-- 포스터 -->
+      <div class="col">
+        <img :src="movie.poster_url ? movie.poster_url : blankImageUrl" style="width:100%" />
       </div>
-      <div class="row">
-        <div class="col">
-          <h4>예고편</h4>
-          <hr />
-          <iframe
-            v-if="movie.video_url"
-            :src="movie.video_url"
-            frameborder="0"
-            style="width:100% ;height:500px;"
-          ></iframe>
+      <!-- 영화 세부 내용 -->
+      <div class="col">
+        <!-- 장르(일단 대표장르만 하나) -->
+        <h3 class="text-center">⭐️ {{movie.score}}</h3>
+        <h5 class="text-center">{{movie.genre.name}}</h5>
+        <!-- 감독'들' -->
+        <div class="text-center mb-2">
+          <h6 class="m-0">감독</h6>
+          <span v-if="!movie.directors">-</span>
+          <span
+            v-else
+            class="mr-1"
+            v-for="director in movie.directors"
+            :key="'director-' + director.id"
+          >{{director.name}}</span>
         </div>
+        <!-- 배우'들' -->
+        <div class="text-center">
+          <h6 class="m-0">배우</h6>
+          <span v-if="!movie.actors">-</span>
+          <span
+            v-else
+            class="mr-1"
+            v-for="actor in movie.actors"
+            :key="'actor-'+ actor.id"
+          >{{actor.name}}</span>
+        </div>
+        <hr />
+        <p>{{movie.description ? movie.description :'-'}}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h4>예고편</h4>
+        <hr />
+        <iframe
+          v-if="movie.video_url"
+          :src="movie.video_url"
+          frameborder="0"
+          style="width:100% ;height:500px;"
+        ></iframe>
       </div>
     </div>
   </div>
@@ -71,11 +68,6 @@ export default {
   props: {
     movie: {
       type: Object
-    }
-  },
-  computed: {
-    loading() {
-      return this.movie ? false : true;
     }
   }
 };
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,20 +1,34 @@
 <template>
   <div>
-    <MovieDetail :movie="movie" />
+    <div v-if="loading">loading..</div>
+    <div v-else>
+      <MovieDetail :movie="movie" />
+      <h5>유저 평가들</h5>
+      <hr />
+      <PostList :posts="posts" />
+      <div v-if="isAuthenticated">
+        <PostForm :movieId="movie.id" @postCreated="onPostCreated" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import MovieDetail from "../components/Movie/MovieDetail";
+import PostForm from "../components/Post/PostForm";
+import PostList from "../components/Post/PostList";
 import axios from "axios";
 export default {
   name: "Movie",
   components: {
-    MovieDetail
+    MovieDetail,
+    PostForm,
+    PostList
   },
   data() {
     return {
-      movie: null
+      movie: null,
+      posts: []
     };
   },
   props: {
@@ -22,16 +36,44 @@ export default {
       type: String
     }
   },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    },
+    loading() {
+      return this.movie ? false : true;
+    }
+  },
+  methods: {
+    getMovie() {
+      const reqUrl = "http://localhost:8000";
+      // 요청 수정 필요
+      axios
+        .get(`${reqUrl}/api/v1/movie/${this.movieId}/`)
+        .then(res => {
+          const { data } = res;
+          this.movie = data;
+        })
+        .catch(err => console.log(err));
+    },
+    getMoviePosts() {
+      const reqUrl = "http://localhost:8000";
+      // 요청 수정 필요
+      axios
+        .get(`${reqUrl}/api/v1/movie/${this.movieId}/posts/`)
+        .then(res => {
+          const { data } = res;
+          this.posts = data;
+        })
+        .catch(err => console.log(err));
+    },
+    onPostCreated(post) {
+      this.posts.push(post);
+    }
+  },
   mounted() {
-    const reqUrl = "http://localhost:8000";
-    // 요청 수정 필요
-    axios
-      .get(`${reqUrl}/api/v1/movie/${this.movieId}/`)
-      .then(res => {
-        const { data } = res;
-        this.movie = data;
-      })
-      .catch(err => console.log(err));
+    this.getMovie();
+    this.getMoviePosts();
   }
 };
 </script>
