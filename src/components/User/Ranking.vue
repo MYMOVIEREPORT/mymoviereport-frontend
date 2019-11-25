5c5514ffdd4eb0fa8b30e72c173460edbc62c3fb
MANDARIN <compurain@naver.com>
2019-11-25 14:53:40 +0900

    랭킹 간단히 생성

---
+++ src/components/User/Ranking.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <RankingList :rankers="rankers" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import RankingList from "../User/RankingList";
+export default {
+  name: "Ranking",
+  components: {
+    RankingList
+  },
+  data() {
+    return {
+      rankers: []
+    };
+  },
+  methods: {
+    getRankers() {
+      const reqUrl = "http://localhost:8000";
+      axios
+        .get(`${reqUrl}/api/v1/user/ranks/`)
+        .then(res => {
+          const { data } = res;
+          this.rankers = data;
+        })
+        .catch(err => console.log(err));
+    }
+  },
+  mounted() {
+    this.getRankers();
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/User/RankingListItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="row ranking-list-wrapper">
+    <div class="col-4">
+      <h4 class="m-0 text-center rank"># {{rank+1}}</h4>
+    </div>
+    <div class="col-8 d-flex align-items-center justify-content-center">
+      <div class="mr-3">
+        <img :src="ranker.thumbnail ? ranker.thumbnail : blankImageUrl" class="rounded-circle" />
+      </div>
+      <h4 class="m-0">{{ranker.username}}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RankingListItem",
+  props: {
+    ranker: {
+      type: Object
+    },
+    rank: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      blankImageUrl:
+        "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.ranking-list-wrapper {
+  display: flex;
+  align-items: center;
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+}
+img {
+  width: 3rem;
+  height: 3rem;
+  object-fit: cover;
+}
+</style>
